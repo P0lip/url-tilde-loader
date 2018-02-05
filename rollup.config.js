56f9f0ac29e61914774c195fe9815bb63e09ef56
Jakub Rożek <jakub1996@gmail.com>
2018-02-05 18:57:24 +0100

    Add support for Node v4 and higher

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   input: './src/loader.js',
   output: {
-    file: `./lib/loader.js`,
+    file: './lib/loader.js',
     format: 'cjs',
-    name: 'tilde-rewriter-loader',
-    sourcemap: true,
+    name: 'url-tilde-loader',
+    sourcemap: false,
   },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
   acorn: {
     allowReserved: true,
     ecmaVersion: 8,
